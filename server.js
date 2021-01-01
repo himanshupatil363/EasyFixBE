a8377ffa7b6c4e800ba64088f28c9d35098d9e35
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2021-01-01 16:58:11 +0530

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//setup express
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+console.log("Starting server");
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
+
+//setup mongoose
+console.log("connecting MongoDB");
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    if(err) return console.error(err);
+    console.log("MongoDB connection established");
+});
