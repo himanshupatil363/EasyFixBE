b766265f287eeba22a2ff8494b0e3be3ff5435e5
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2021-01-18 19:23:03 +0530

    kk

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: { type:String, required: true },
     emailid: { type:String , required:true, unique:true },
-    pwd: { type:String, required:true },
+    pwd: { type:String, required:true ,minlength: 5},
     photo: { type:String, required:false},
     city: { type:String, required:false}
 });
--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 //setup express
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9999;
 console.log("Starting server");
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
-//setup routes
-app.use("/user", require("./routes/userroutes"));
-app.use("/provider", require("./routes/providerroutes"));
-app.use("/admin", require("./routes/adminroutes"));
+
 //setup mongoose
 console.log("connecting MongoDB");
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
