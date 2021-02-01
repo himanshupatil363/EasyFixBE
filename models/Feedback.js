d8b07d02fce4c2cc53724ee97d9a59228cf6aa2b
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2021-02-01 19:07:49 +0530

    okok

--- models/Feedback.js
+++ models/Feedback.js
@@ -7,6 +7,7 @@ const FeedbackSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "please provide a email"],
+        unique:false,
         match: [
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             "Please provide a valid email"
