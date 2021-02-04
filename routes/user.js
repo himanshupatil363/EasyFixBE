7ea2d26a1df95bfd8bb74eace76b6a99b0fe901d
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2021-02-04 22:00:13 +0530

    userroute

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 const {
     feedback
 } = require('../controllers/user')
 router.route("/feedback").post(feedback);
+router.get("/all", async (req, res) => {
+    try {
+        const allUser = await User.find();
+        res.json(allUser)
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
+});
 module.exports = router;
