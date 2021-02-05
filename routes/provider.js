cf2e65631188c8c5fd85f62e92984d67b8a72d7a
brinda chanchad <brinda@brindas-MacBook-Pro.local>
2021-02-05 17:13:42 +0530

    added routes

--- routes/provider.js
+++ routes/provider.js
@@ -5,11 +5,21 @@ const {
     login,
     forgotpassword,
     resetpassword
-} = require('../controllers/pauth')
+} = require('../controllers/pauth');
+const Provider = require('../models/Provider');
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/forgotpassword").post(forgotpassword);
 router.route("/resetpassword/:resetToken").put(resetpassword);
-
+router.get("/all", async (req, res) => {
+    try {
+        const allProvider = await Provider.find();
+        res.json(allProvider)
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
+});
 module.exports = router;
